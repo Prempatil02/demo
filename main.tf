71c682756dde4c9000674afcab01380a5297dff2
prempatil02 <pramod.taywade993@gmail.com>
2022-06-15 14:33:56 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "aws_vpc" "main_vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main_vpc"
+  }
+}
+resource "aws_subnet" "main_subnet" {
+  vpc_id     = aws_vpc.main_vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main_subnet"
+  }
+}
